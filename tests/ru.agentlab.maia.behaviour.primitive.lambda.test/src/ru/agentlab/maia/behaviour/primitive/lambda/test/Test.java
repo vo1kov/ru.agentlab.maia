5cad4d6d24072be3f812530ad17baa764f1cabc5
Dmitriy Shishkin <shishkindimon@gmail.com>
2015-12-25 05:33:38 +0400

    * change uml

--- tests/ru.agentlab.maia.behaviour.primitive.lambda.test/src/ru/agentlab/maia/behaviour/primitive/lambda/test/Test.java
+++ tests/ru.agentlab.maia.behaviour.primitive.lambda.test/src/ru/agentlab/maia/behaviour/primitive/lambda/test/Test.java
@@ -2,12 +2,14 @@ package ru.agentlab.maia.behaviour.primitive.lambda.test;
 
 import java.util.Arrays;
 
+import ru.agentlab.maia.behaviour.Behaviour;
 import ru.agentlab.maia.behaviour.primitive.lambda.BehaviourPrimitiveLambda;
 
 public class Test {
 
 	public static void main(String[] args) {
 		BehaviourPrimitiveLambda behaviour = new BehaviourPrimitiveLambda();
+		behaviour.addInput(new Behaviour.Parameter<String>("string", String.class));
 		behaviour.setLambda(a -> new Object[] { Arrays.asList(a).stream().count() });
 	}
 
