8ae7331812ef4d5459d9ffab89594508fc77b9c8
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-05-13 20:18:21 +0400

    add hello world

---
+++ examples/ru.agentlab.maia.examples/src/ru/agentlab/maia/examples/HelloWorld.java
@@ -0,0 +1,33 @@
+package ru.agentlab.maia.examples;
+
+import static ru.agentlab.maia.EventType.AGENT_GOAL_ADDED;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import ru.agentlab.maia.IGoalBase;
+import ru.agentlab.maia.IMessage;
+import ru.agentlab.maia.annotation.Trigger;
+import ru.agentlab.maia.messaging.AclMessage;
+import ru.agentlab.maia.messaging.IMessageDeliveryService;
+
+public class HelloWorld {
+
+	@Inject
+	IMessageDeliveryService messaging;
+
+	@Inject
+	IGoalBase goalBase;
+
+	@PostConstruct
+	public void setup() {
+		goalBase.addGoal("init");
+	}
+
+	@Trigger(type = AGENT_GOAL_ADDED, template = "init")
+	public void onSomeClassified() {
+		IMessage message = new AclMessage();
+		message.setContent("Hello World");
+		messaging.send(message);
+	}
+}
