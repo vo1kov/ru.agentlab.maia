5edce1ca38c49babb0332b44cadf9d50b91f1340
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-05-17 17:41:21 +0400

    add OSGI Bundle agent roles

---
+++ tools/ru.agentlab.maia.admin.bundles/src/ru/agentlab/maia/admin/bundles/BundleManager.java
@@ -0,0 +1,30 @@
+package ru.agentlab.maia.admin.bundles;
+
+import javax.inject.Named;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+
+import ru.agentlab.maia.admin.bundles.internal.Activator;
+import ru.agentlab.maia.annotation.GoalAdded;
+import ru.agentlab.maia.annotation.HaveBelief;
+import ru.agentlab.maia.annotation.Prefix;
+
+@Prefix(name = "osgi", namespace = "http://www.agentlab.ru/ontologies/osgi")
+public class BundleManager {
+
+	@GoalAdded("?bundle osgi:hasBundleState osgi:STARTRED")
+	@HaveBelief("?bundle osgi:hasBundleState osgi:INSTALLED")
+	@HaveBelief("?bundle osgi:hasBundleId ?bundleId")
+	public void startBundle(@Named("bundleId") long bundleId) throws BundleException {
+		Bundle bundle = Activator.context.getBundle(bundleId);
+		bundle.start();
+	}
+
+	@GoalAdded("?bundle osgi:hasBundleState osgi:INSTALLED")
+	@HaveBelief("?bundle osgi:hasBundleLocation ?location")
+	public void installBundle(@Named("location") String location) throws BundleException {
+		Activator.context.installBundle(location);
+	}
+
+}
---
+++ tools/ru.agentlab.maia.admin.bundles/src/ru/agentlab/maia/admin/bundles/BundleSubscriptionResponder.java
@@ -0,0 +1,89 @@
+package ru.agentlab.maia.admin.bundles;
+
+import static ru.agentlab.maia.IMessage.AGREE;
+import static ru.agentlab.maia.IMessage.CANCEL;
+import static ru.agentlab.maia.IMessage.FAILURE;
+import static ru.agentlab.maia.IMessage.INFORM;
+import static ru.agentlab.maia.IMessage.REFUSE;
+import static ru.agentlab.maia.IMessage.SUBSCRIBE;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import ru.agentlab.maia.IMessage;
+import ru.agentlab.maia.annotation.BeliefAdded;
+import ru.agentlab.maia.annotation.HaveBelief;
+import ru.agentlab.maia.annotation.MessageAdded;
+import ru.agentlab.maia.annotation.Prefix;
+import ru.agentlab.maia.messaging.AclMessage;
+import ru.agentlab.maia.messaging.IMessageDeliveryService;
+
+public class BundleSubscriptionResponder {
+
+	private Map<UUID, String> subscriberConverdationIds = new HashMap<>();
+
+	private Map<String, List<UUID>> subscriberProperties = new HashMap<>();
+
+	@Inject
+	UUID uuid;
+
+	@Inject
+	IMessageDeliveryService mts;
+
+	@MessageAdded(performative = SUBSCRIBE, protocol = "FIPA_SUBSCRIBE")
+	public void onSubscribe(IMessage message) {
+		UUID sender = message.getSender();
+		String conversationId = message.getConversationId();
+		subscriberConverdationIds.put(sender, conversationId);
+		String property = message.getContent();
+		if (property == null) {
+			reply(message, REFUSE);
+		}
+		List<UUID> receivers = subscriberProperties.get(property);
+		if (receivers == null) {
+			receivers = new ArrayList<>();
+			subscriberProperties.put(property, receivers);
+		}
+		receivers.add(sender);
+		reply(message, AGREE);
+	}
+
+	@MessageAdded(performative = CANCEL, protocol = "FIPA_SUBSCRIBE")
+	public void onCancel(IMessage message) {
+		UUID sender = message.getSender();
+		String conversationId = message.getConversationId();
+		if (subscriberConverdationIds.get(sender) == conversationId) {
+			subscriberConverdationIds.remove(sender);
+			reply(message, INFORM);
+		} else {
+			reply(message, FAILURE);
+		}
+	}
+
+	@BeliefAdded("?bundle ?property ?value")
+	@HaveBelief("?bundle rdf:type osgi:Bundle")
+	@Prefix(name = "osgi", namespace = "http://www.agentlab.ru/ontologies/osgi")
+	public void onPropertyChanged(@Named("bundle") String bundle, @Named("property") String property,
+			@Named("value") String value) {
+		IMessage message = new AclMessage();
+		message.setPerformative(INFORM);
+		List<UUID> receivers = subscriberProperties.get(property);
+		message.setReceivers(receivers);
+		message.setSender(uuid);
+		message.setContent(bundle + " " + property + " " + value);
+		mts.send(message);
+	}
+
+	private void reply(IMessage message, String performative) {
+		IMessage reply = message.createReply();
+		reply.setPerformative(performative);
+		mts.send(reply);
+	}
+
+}
