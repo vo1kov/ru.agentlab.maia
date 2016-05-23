2d8be68ba521b61c7cfe4734cc75b2b4b0891846
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-05-23 04:10:59 +0400

    add numeration, javadoc and vary namespaces

--- tests/ru.agentlab.maia.agent.test/src/ru/agentlab/maia/agent/converter/ConverterSplitDatatypeLiteralTest.java
+++ tests/ru.agentlab.maia.agent.test/src/ru/agentlab/maia/agent/converter/ConverterSplitDatatypeLiteralTest.java
@@ -21,15 +21,22 @@ import org.junit.runners.Parameterized.Parameters;
 import org.semanticweb.owlapi.vocab.Namespaces;
 
 /**
+ * Test method which split literal into 3 parts:
+ * <ol>
+ * <li>Literal value;
+ * <li>Literal language;
+ * <li>Literal datatype;
+ * </ol>
+ * 
  * @author Dmitriy Shishkin <shishkindimon@gmail.com>
  */
 @RunWith(Parameterized.class)
 public class ConverterSplitDatatypeLiteralTest {
 
-	final static String RDF = Namespaces.RDF.toString();
-	final static String RDFS = Namespaces.RDFS.toString();
-	final static String OWL = Namespaces.OWL.toString();
-	final static String XSD = Namespaces.XSD.toString();
+	private final static String RDF = Namespaces.RDF.toString();
+	private final static String RDFS = Namespaces.RDFS.toString();
+	private final static String OWL = Namespaces.OWL.toString();
+	private final static String XSD = Namespaces.XSD.toString();
 
 	// @formatter:off
 	// Name is not working because some of the test strings have \r\n symbols
@@ -41,32 +48,34 @@ public class ConverterSplitDatatypeLiteralTest {
 			 *---------------------------------------------------------------------------------------------------------------------------------------------*/
 			// Empty word
 			/*  0 */ { "", 									"",				null, 		null },							// test empty string
-			/*  0 */ { "^^rdf:XMLLiteral", 					"",				null, 		"rdf:XMLLiteral" },				// test only datatype
-			/*  0 */ { "^^<" + RDF + "XMLLiteral>", 		"",				null, 		"<" + RDF + "XMLLiteral>" },	// test only full datatype
-			/*  0 */ { "@ru", 								"",				"ru", 		null },							// test only language
-			/*  0 */ { "@ru-RU", 							"",				"ru-RU", 	null },							// test only language
-			/*  0 */ { "@en^^rdf:XMLLiteral", 				"",				"en", 		"rdf:XMLLiteral" },				// test language and datatype
-			/*  0 */ { "@en^^<" + RDF + "XMLLiteral>", 		"",				"en", 		"<" + RDF + "XMLLiteral>" },	// test language and full datatype
+			/*  1 */ { "^^rdf:XMLLiteral", 					"",				null, 		"rdf:XMLLiteral" },				// test only datatype
+			/*  2 */ { "^^<" + RDF + "XMLLiteral>", 		"",				null, 		"<" + RDF + "XMLLiteral>" },	// test only full datatype
+			/*  3 */ { "@ru", 								"",				"ru", 		null },							// test only language
+			/*  4 */ { "@ru-RU", 							"",				"ru-RU", 	null },							// test only language
+			/*  5 */ { "@en^^rdf:XMLLiteral", 				"",				"en", 		"rdf:XMLLiteral" },				// test language and datatype
+			/*  6 */ { "@en^^<" + RDFS + "Literal>", 		"",				"en", 		"<" + RDFS + "Literal>" },		// test language and full datatype
 			// Single word
-			/*  0 */ { "some", 								"some",			null, 		null },							// test single word string
-			/*  0 */ { "test^^rdf:XMLLiteral", 				"test",			null, 		"rdf:XMLLiteral" },				// test single word and datatype
-			/*  0 */ { "test^^<" + RDF + "XMLLiteral>", 	"test",			null, 		"<" + RDF + "XMLLiteral>" },	// test single word and full datatype
-			/*  0 */ { "тест@ru", 							"тест",			"ru", 		null },							// test single word and language
-			/*  0 */ { "тест@ru-RU", 						"тест",			"ru-RU", 	null },							// test single word and language
-			/*  0 */ { "test@en^^rdf:XMLLiteral", 			"test",			"en", 		"rdf:XMLLiteral" },				// test single word, language and datatype
-			/*  0 */ { "test@en^^<" + RDF + "XMLLiteral>", 	"test",			"en", 		"<" + RDF + "XMLLiteral>" },	// test single word, language and full datatype
+			/*  7 */ { "some", 								"some",			null, 		null },							// test single word string
+			/*  8 */ { "test^^rdf:XMLLiteral", 				"test",			null, 		"rdf:XMLLiteral" },				// test single word and datatype
+			/*  9 */ { "test^^<" + RDF + "XMLLiteral>", 	"test",			null, 		"<" + RDF + "XMLLiteral>" },	// test single word and full datatype
+			/* 10 */ { "тест@ru", 							"тест",			"ru", 		null },							// test single word and language
+			/* 11 */ { "тест@ru-RU", 						"тест",			"ru-RU", 	null },							// test single word and language
+			/* 12 */ { "test@en^^rdf:XMLLiteral", 			"test",			"en", 		"rdf:XMLLiteral" },				// test single word, language and datatype
+			/* 13 */ { "test@en^^<" + RDF + "XMLLiteral>", 	"test",			"en", 		"<" + RDF + "XMLLiteral>" },	// test single word, language and full datatype
 			// Multiple words
-			/*  0 */ { "some more", 						"some more",	null, 		null },							// test multiple words string
-			/*  0 */ { "test w 2^^rdf:XMLLiteral", 			"test w 2",		null, 		"rdf:XMLLiteral" },				// test multiple words and datatype
-			/*  0 */ { "test^^<" + RDF + "XMLLiteral>", 	"test",			null, 		"<" + RDF + "XMLLiteral>" },	// test multiple words and full datatype
-			/*  0 */ { "тест 2 3@ru", 						"тест 2 3",		"ru", 		null },							// test multiple words and language
-			/*  0 */ { "тест qwe@ru-RU", 					"тест qwe",		"ru-RU", 	null },							// test multiple words and language
-			/*  0 */ { "test@en^^rdf:XMLLiteral", 			"test",			"en", 		"rdf:XMLLiteral" },				// test multiple words, language and datatype
-			/*  0 */ { "test@en^^<" + RDF + "XMLLiteral>", 	"test",			"en", 		"<" + RDF + "XMLLiteral>" },	// test multiple words, language and full datatype
+			/* 14 */ { "some more", 						"some more",	null, 		null },							// test multiple words string
+			/* 15 */ { "test w 2^^rdf:XMLLiteral", 			"test w 2",		null, 		"rdf:XMLLiteral" },				// test multiple words and datatype
+			/* 16 */ { "test^^<" + OWL + "XMLLiteral>", 	"test",			null, 		"<" + OWL + "XMLLiteral>" },	// test multiple words and full datatype
+			/* 17 */ { "тест 2 3@ru", 						"тест 2 3",		"ru", 		null },							// test multiple words and language
+			/* 18 */ { "тест qwe@ru-RU", 					"тест qwe",		"ru-RU", 	null },							// test multiple words and language
+			/* 19 */ { "test@en^^rdf:XMLLiteral", 			"test",			"en", 		"rdf:XMLLiteral" },				// test multiple words, language and datatype
+			/* 20 */ { "test@en^^<" + XSD + "XMLLiteral>", 	"test",			"en", 		"<" + XSD + "XMLLiteral>" },	// test multiple words, language and full datatype
 			// Special characters
-			/*  0 */ { "som\r\n\tmore", 					"som\r\n\tmore",null, 		null },							// test \r\n\t in literal
-			/*  0 */ { "2^2^^owl:real", 					"2^2",			null, 		"owl:real" },					// test ^ in literal
-			/*  0 */ { "email@dot.com@en^^xsd:real", 		"email@dot.com","en", 		"xsd:real" },					// test @ in literal
+			/* 21 */ { "som\r\n\tmore", 					"som\r\n\tmore",null, 		null },							// test \r\n\t in literal
+			/* 22 */ { "2^2^^owl:real", 					"2^2",			null, 		"owl:real" },					// test ^ in literal
+			/* 23 */ { "2^^2^^owl:real", 					"2^^2",			null, 		"owl:real" },					// test ^^ in literal
+			/* 24 */ { "2^^2@^^owl:real", 					"2^^2",			"", 		"owl:real" },					// test ^^ and @ in literal
+			/* 25 */ { "email@dot.com@en^^xsd:real", 		"email@dot.com","en", 		"xsd:real" },					// test @ in literal
 		});
 	}
 	// @formatter:on
