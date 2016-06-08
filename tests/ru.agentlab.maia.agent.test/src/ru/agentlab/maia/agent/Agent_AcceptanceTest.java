3588a103ed58c4a17e464ca2066df372e13146b7
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-06-08 15:08:48 +0300

    Rfactor test. Move role into test class as nested

--- tests/ru.agentlab.maia.agent.test/src/ru/agentlab/maia/agent/Agent_AcceptanceTest.java
+++ tests/ru.agentlab.maia.agent.test/src/ru/agentlab/maia/agent/Agent_AcceptanceTest.java
@@ -3,14 +3,23 @@ package ru.agentlab.maia.agent;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.vocab.Namespaces;
 
+import ru.agentlab.maia.IAgent;
 import ru.agentlab.maia.IContainer;
-import ru.agentlab.maia.agent.doubles.AnnTest;
+import ru.agentlab.maia.annotation.event.AddedDataPropertyAssertion;
+import ru.agentlab.maia.annotation.state.HaveClassAssertion;
+import ru.agentlab.maia.annotation.state.HaveSubClassOf;
 import ru.agentlab.maia.container.Container;
 import ru.agentlab.maia.event.AddedDataPropertyAssertionEvent;
 import ru.agentlab.maia.exception.ContainerException;
@@ -31,7 +40,7 @@ public class Agent_AcceptanceTest {
 		container.put(String.class, "TEST");
 		Agent agent = new Agent();
 		agent.deployTo(container);
-		agent.addRole(AnnTest.class);
+		agent.addRole(TestRole.class);
 		IntStream.range(0, 500).forEach(i -> {
 			agent.eventQueue.offer(new AddedDataPropertyAssertionEvent(factory.getOWLDataPropertyAssertionAxiom(
 					factory.getOWLDataProperty(IRI.create(Namespaces.RDF.toString(), "hasProperty")),
@@ -44,4 +53,32 @@ public class Agent_AcceptanceTest {
 
 	}
 
+	public static class TestRole {
+
+		@Inject
+		IAgent agent;
+
+		@PostConstruct
+		public void init() {
+			System.out.println(agent.getUuid());
+		}
+
+		@AddedDataPropertyAssertion("rdf:ind rdf:hasProperty 2^^xsd:integer")
+		@HaveSubClassOf("owl:Thing rdf:Some")
+		@HaveClassAssertion("rdf:Some rdf:ind")
+		public void exe() {
+			System.out.println("WORKS");
+
+		}
+
+		@AddedDataPropertyAssertion("?ind ?property 2^^xsd:integer")
+		@HaveSubClassOf("owl:Thing rdf:Some")
+		@HaveClassAssertion("rdf:Some rdf:ind")
+		public void exe2(@Named("property") OWLDataProperty property, @Named("ind") OWLIndividual ind) {
+			System.out.println("WORKS2" + property.toString());
+			System.out.println("WORKS2" + ind.toString());
+
+		}
+
+	}
 }
