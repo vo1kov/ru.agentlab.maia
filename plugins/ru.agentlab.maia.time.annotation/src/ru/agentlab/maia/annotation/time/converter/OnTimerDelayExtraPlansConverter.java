4dae10c9c31c7fe6dc4297c0d9fd6d79034f846d
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-07-20 19:33:15 +0400

    Rename projects
    * Add time project

---
+++ plugins/ru.agentlab.maia.time.annotation/src/ru/agentlab/maia/annotation/time/converter/OnTimerDelayExtraPlansConverter.java
@@ -0,0 +1,55 @@
+package ru.agentlab.maia.annotation.time.converter;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import javax.inject.Inject;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+import ru.agentlab.maia.IAgent;
+import ru.agentlab.maia.IPlan;
+import ru.agentlab.maia.agent.Plan;
+import ru.agentlab.maia.annotation.IExtraPlansConverter;
+import ru.agentlab.maia.annotation.time.OnTimerDelay;
+import ru.agentlab.maia.event.AgentStartedEvent;
+import ru.agentlab.maia.event.AgentStoppedEvent;
+import ru.agentlab.maia.time.TimerEvent;
+
+public class OnTimerDelayExtraPlansConverter implements IExtraPlansConverter {
+
+	@Inject
+	ScheduledExecutorService scheduler;
+
+	@Inject
+	IAgent agent;
+
+	@Override
+	public Multimap<Class<?>, IPlan> getPlans(Object role, Method method, Annotation annotation,
+			Map<String, Object> customData) {
+		OnTimerDelay onTimerDelay = (OnTimerDelay) annotation;
+
+		final UUID eventKey = (UUID) customData.get(OnTimerXXXEventMatcherConverter.EVENT_KEY);
+		final ScheduledFuture<?>[] futures = new ScheduledFuture[1];
+
+		Runnable onStartPlan = () -> {
+			futures[0] = scheduler.schedule(() -> agent.fireExternalEvent(new TimerEvent(eventKey)),
+					onTimerDelay.value(), onTimerDelay.unit());
+		};
+
+		Runnable onStopPlan = () -> {
+			futures[0].cancel(true);
+		};
+
+		Multimap<Class<?>, IPlan> result = ArrayListMultimap.create();
+		result.put(AgentStartedEvent.class, new Plan(role, onStartPlan));
+		result.put(AgentStoppedEvent.class, new Plan(role, onStopPlan));
+		return result;
+	}
+
+}
---
+++ plugins/ru.agentlab.maia.time.annotation/src/ru/agentlab/maia/annotation/time/converter/OnTimerFixedPeriodExtraPlansConverter.java
@@ -0,0 +1,55 @@
+package ru.agentlab.maia.annotation.time.converter;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import javax.inject.Inject;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+import ru.agentlab.maia.IAgent;
+import ru.agentlab.maia.IPlan;
+import ru.agentlab.maia.agent.Plan;
+import ru.agentlab.maia.annotation.IExtraPlansConverter;
+import ru.agentlab.maia.annotation.time.OnTimerFixedPeriod;
+import ru.agentlab.maia.event.AgentStartedEvent;
+import ru.agentlab.maia.event.AgentStoppedEvent;
+import ru.agentlab.maia.time.TimerEvent;
+
+public class OnTimerFixedPeriodExtraPlansConverter implements IExtraPlansConverter {
+
+	@Inject
+	ScheduledExecutorService scheduler;
+
+	@Inject
+	IAgent agent;
+
+	@Override
+	public Multimap<Class<?>, IPlan> getPlans(Object role, Method method, Annotation annotation,
+			Map<String, Object> customData) {
+		OnTimerFixedPeriod onTimerDelay = (OnTimerFixedPeriod) annotation;
+
+		final UUID eventKey = (UUID) customData.get(OnTimerXXXEventMatcherConverter.EVENT_KEY);
+		final ScheduledFuture<?>[] futures = new ScheduledFuture[1];
+
+		Runnable onStartPlan = () -> {
+			futures[0] = scheduler.scheduleWithFixedDelay(() -> agent.fireExternalEvent(new TimerEvent(eventKey)),
+					onTimerDelay.delay(), onTimerDelay.value(), onTimerDelay.unit());
+		};
+
+		Runnable onStopPlan = () -> {
+			futures[0].cancel(true);
+		};
+
+		Multimap<Class<?>, IPlan> result = ArrayListMultimap.create();
+		result.put(AgentStartedEvent.class, new Plan(role, onStartPlan));
+		result.put(AgentStoppedEvent.class, new Plan(role, onStopPlan));
+		return result;
+	}
+
+}
