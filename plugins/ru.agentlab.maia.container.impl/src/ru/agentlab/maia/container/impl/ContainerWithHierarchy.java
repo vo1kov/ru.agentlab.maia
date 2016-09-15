293ef54032fd30b3299f6d55e7964047a19a3767
Dmitriy Shishkin <shishkindimon@gmail.com>
2016-09-16 02:16:35 +0300

    Add lock for class keys

--- plugins/ru.agentlab.maia.container.impl/src/ru/agentlab/maia/container/impl/ContainerWithHierarchy.java
+++ plugins/ru.agentlab.maia.container.impl/src/ru/agentlab/maia/container/impl/ContainerWithHierarchy.java
@@ -11,18 +11,20 @@ package ru.agentlab.maia.container.impl;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.commons.lang.ClassUtils;
 
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
 
 /**
  * 
@@ -30,12 +32,13 @@ import com.google.common.collect.Multimaps;
  */
 public class ContainerWithHierarchy extends AbstractContainer {
 
-	protected final Multimap<Class<?>, Object> valuesBySuperClass = Multimaps
-		.synchronizedSetMultimap(LinkedHashMultimap.create());
+	protected final Multimap<Class<?>, Object> valuesBySuperClass = LinkedHashMultimap.create();
 
-	protected final Map<Class<?>, Object> valuesByClass = new ConcurrentHashMap<>();
+	protected final Map<Class<?>, Object> valuesByClass = new HashMap<>();
 
 	protected final Map<String, Object> valuesByString = new ConcurrentHashMap<>();
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
 	{
 		put(this);
@@ -52,17 +55,21 @@ public class ContainerWithHierarchy extends AbstractContainer {
 	@SuppressWarnings("unchecked")
 	public <T> T getLocal(Class<T> key) {
 		checkNotNull(key, "Key class should be non null");
+		lock.readLock().lock();
+		T result;
 		Object service = valuesByClass.get(key);
 		if (service == null) {
 			Iterator<Object> iterator = valuesBySuperClass.get(key).iterator();
 			if (iterator.hasNext()) {
-				return (T) iterator.next();
+				result = (T) iterator.next();
 			} else {
-				return null;
+				result = null;
 			}
 		} else {
-			return (T) service;
+			result = (T) service;
 		}
+		lock.readLock().unlock();
+		return result;
 	}
 
 	@Override
@@ -78,12 +85,13 @@ public class ContainerWithHierarchy extends AbstractContainer {
 		checkNotNull(service, "Service should be non null");
 		List<Class<?>> superinterfaces = ClassUtils.getAllInterfaces(key);
 		List<Class<?>> superclasses = ClassUtils.getAllSuperclasses(key);
+		lock.writeLock().lock();
 		valuesByClass.put(key, service);
 		valuesBySuperClass.put(key, service);
 		superinterfaces.stream().forEach(superinterface -> valuesBySuperClass.put(superinterface, service));
 		superclasses.stream().filter(superinterface -> superinterface != Object.class).forEach(
 			superclass -> valuesBySuperClass.put(superclass, service));
-
+		lock.writeLock().unlock();
 		return service;
 	}
 
@@ -94,11 +102,13 @@ public class ContainerWithHierarchy extends AbstractContainer {
 		Class<? extends Object> key = service.getClass();
 		List<Class<?>> superinterfaces = ClassUtils.getAllInterfaces(key);
 		List<Class<?>> superclasses = ClassUtils.getAllSuperclasses(key);
+		lock.writeLock().lock();
 		valuesByClass.put(key, service);
 		valuesBySuperClass.put(key, service);
 		superinterfaces.stream().forEach(superinterface -> valuesBySuperClass.put(superinterface, service));
 		superclasses.stream().filter(superinterface -> superinterface != Object.class).forEach(
 			superclass -> valuesBySuperClass.put(superclass, service));
+		lock.writeLock().unlock();
 		return service;
 	}
 
@@ -115,6 +125,7 @@ public class ContainerWithHierarchy extends AbstractContainer {
 		Class<? extends Object> clazz = service.getClass();
 		List<Class<?>> superinterfaces = ClassUtils.getAllInterfaces(clazz);
 		List<Class<?>> superclasses = ClassUtils.getAllSuperclasses(clazz);
+		lock.writeLock().lock();
 		valuesByClass.remove(clazz, service);
 		valuesBySuperClass.remove(clazz, service);
 		superinterfaces.stream().forEach(superinterface -> {
@@ -125,36 +136,49 @@ public class ContainerWithHierarchy extends AbstractContainer {
 			valuesBySuperClass.remove(superclass, service);
 			valuesByClass.remove(superclass, service);
 		});
+		lock.writeLock().unlock();
 		return null;
 	}
 
 	@Override
 	public Object remove(Class<?> key) {
 		checkNotNull(key, "Key class should be non null");
+		lock.writeLock().lock();
 		valuesByClass.remove(key);
-		return valuesBySuperClass.removeAll(key);
+		valuesBySuperClass.removeAll(key);
+		lock.writeLock().unlock();
+		return null;
 	}
 
 	@Override
 	public Set<Object> getKeySet() {
 		Set<Object> result = new HashSet<>();
-		result.addAll(valuesByString.keySet());
+		lock.readLock().lock();
+		result.addAll(valuesByClass.keySet());
 		result.addAll(valuesBySuperClass.keySet());
+		lock.readLock().unlock();
+		result.addAll(valuesByString.keySet());
 		return result;
 	}
 
 	@Override
 	public Collection<Object> values() {
 		Set<Object> result = new HashSet<>();
-		result.addAll(valuesByString.values());
+		lock.readLock().lock();
+		result.addAll(valuesByClass.values());
 		result.addAll(valuesBySuperClass.values());
+		lock.readLock().unlock();
+		result.addAll(valuesByString.values());
 		return result;
 	}
 
 	@Override
 	public boolean clear() {
-		valuesByString.clear();
+		lock.writeLock().lock();
+		valuesByClass.clear();
 		valuesBySuperClass.clear();
+		lock.writeLock().unlock();
+		valuesByString.clear();
 		return true;
 	}
 
